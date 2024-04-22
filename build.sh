4f879e25bc91ec474d3871d5ac97eb8875c549c6
julenbhy <julenbhy@example.com>
2024-04-22 15:44:24 +0200

    Fix build and run scripts for WASI support

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-
-cd cpython
 # Change max-memory to 4294901760 to fix the error: "wasm-ld: error: maximum memory too small, 20971520 bytes needed"
-sed -i 's/--max-memory=10485760/--max-memory=4294901760/' configure
-python3 ./Tools/wasm/wasm_build.py wasi-threads
+sed -i 's/--max-memory=10485760/--max-memory=4294901760/' cpython/configure
+python3 ./cpython/Tools/wasm/wasm_build.py wasi-threads
 
 
 mkdir cpython/pyperformance/
 cp -r pyperformance/pyperformance/data-files/benchmarks/* cpython/pyperformance/
 
-cp -r ..pyperf/pyperf cpython/Lib/
+
+# Solve executable not found error
+sed -i 's/args = self.argparser.parse_args(args)/args = self.argparser.parse_args(args)\n        args.python="builddir\/wasi-threads\/python.wasm"/' pyperf/pyperf/_runner.py
+cp -r pyperf/pyperf cpython/Lib/
--- run_python.sh
+++ run_python.sh
@@ -1,5 +1,14 @@
-WASMTIME=/home/julen/.wasmtime/bin/wasmtime
 
+#DIR="/home/julen/Cloudlab/python/WASI-python-benchmarks/cpython::/"
+#ENV="PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13"
+#EXE="/home/julen/Cloudlab/python/WASI-python-benchmarks/cpython/builddir/wasi-threads/python.wasm"
 
-#exec $WASMTIME run --wasm max-wasm-stack=8388608 --wasm threads=y --wasi threads=y --dir /home/julen/Cloudlab/python/cpython::/ --env PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13-pydebug /home/julen/Cloudlab/python/cpython/builddir/wasi-threads/python.wasm "$@"
-exec $WASMTIME run --wasm max-wasm-stack=8388608 --wasm threads=y --wasi threads=y --dir /home/julen/Cloudlab/python/WASI-python-benchmarks::/ --env PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13-pydebug /home/julen/Cloudlab/python/WASI-python-benchmarks/builddir/wasi-threads/python.wasm "$@"
+CUR_DIR=$(pwd)
+DIR="$CUR_DIR/cpython::/"
+ENV="PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13"
+EXE="$CUR_DIR/cpython/builddir/wasi-threads/python.wasm"
+
+#WORKING:
+#exec /home/julen/.wasmtime/bin/wasmtime run -S threads --wasm max-wasm-stack=8388608 --wasi preview2 --dir /home/julen/Cloudlab/python/WASI-python-benchmarks/cpython::/ --env PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13 /home/julen/Cloudlab/python/WASI-python-benchmarks/cpython/builddir/wasi-threads/python.wasm "$@"
+
+exec /home/julen/.wasmtime/bin/wasmtime run -S threads --wasm max-wasm-stack=8388608 --wasi preview2 --dir $DIR --env $ENV $EXE "$@"
