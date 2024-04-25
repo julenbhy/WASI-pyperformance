b1277f832c322a791e8ee5c92aa20cce2a165511
julenbhy <julenbhy@gmail.com>
2024-04-25 13:58:39 +0200

    Update run_python.sh script for relative paths and error handling

--- run_python.sh
+++ run_python.sh
@@ -1,10 +1,14 @@
-
-CUR_DIR=$(pwd)
-DIR="$CUR_DIR/cpython::/"
+DIR=".::/"
 ENV="PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13"
-EXE="$CUR_DIR/cpython/builddir/wasi-threads/python.wasm"
+EXE="./builddir/wasi-threads/python.wasm"
 
-#WORKING:
-#exec /home/julen/.wasmtime/bin/wasmtime run -S threads --wasm max-wasm-stack=8388608 --wasi preview2 --dir /home/julen/Cloudlab/python/WASI-python-benchmarks/cpython::/ --env PYTHONPATH=/builddir/wasi-threads/build/lib.wasi-wasm32-3.13 /home/julen/Cloudlab/python/WASI-python-benchmarks/cpython/builddir/wasi-threads/python.wasm "$@"
 
-exec /home/julen/.wasmtime/bin/wasmtime run -S threads --wasm max-wasm-stack=8388608 --dir $DIR --env $ENV $EXE "$@"
+cd cpython
+
+# Run previous command, if it fails, print error and exit
+if ! wasmtime run -S threads --wasm max-wasm-stack=8388608 --dir $DIR --env $ENV $EXE "$@"; then
+    echo -e "\e[31mError: wasmtime failed\e[0m"
+    echo "      Files clould be mapped to a different path inside the WASI host"
+    echo "      set path/to/your/script.py from inside cpython directory"
+    exit 1
+fi
